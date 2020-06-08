30fb147c963d534bbad14e5538a2e89a623a8d42
gayatrijagtap <gdjagtap1999@gmail.com>
2020-06-08 10:48:00 +0530

    [G_70] Gayatri | Implement removing temporary constant changes to patients/find

--- src/main/java/in/projecteka/gateway/user/UserController.java
+++ src/main/java/in/projecteka/gateway/user/UserController.java
@@ -1,10 +1,12 @@
 package in.projecteka.gateway.user;
 
+import in.projecteka.gateway.clients.Caller;
 import in.projecteka.gateway.clients.PatientSearchServiceClient;
 import in.projecteka.gateway.common.RequestOrchestrator;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,8 +22,9 @@ public class UserController {
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/v1/patients/find")
     public Mono<Void> findPatient(HttpEntity<String> requestEntity) {
-        Mono<Void> toBeFiredAndForgotten = patientSearchRequestOrchestrator.processRequest(requestEntity, X_CM_ID);
-        toBeFiredAndForgotten.subscribe();
-        return Mono.empty();
+        return ReactiveSecurityContextHolder.getContext()
+                .map(securityContext -> (Caller) securityContext.getAuthentication().getPrincipal())
+                .map(Caller::getClientId)
+                .flatMap(clientId -> patientSearchRequestOrchestrator.processRequest(requestEntity, X_CM_ID, clientId));
     }
 }
--- src/test/java/in/projecteka/gateway/user/UserControllerTest.java
+++ src/test/java/in/projecteka/gateway/user/UserControllerTest.java
@@ -2,26 +2,32 @@ package in.projecteka.gateway.user;
 
 import com.nimbusds.jose.jwk.JWKSet;
 import in.projecteka.gateway.clients.PatientSearchServiceClient;
+import in.projecteka.gateway.common.CentralRegistryTokenVerifier;
 import in.projecteka.gateway.common.RequestOrchestrator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
-import reactor.core.publisher.Mono;
-
-import java.time.Duration;
 
 import static in.projecteka.gateway.common.Constants.X_CM_ID;
+import static in.projecteka.gateway.testcommon.TestBuilders.caller;
+import static in.projecteka.gateway.testcommon.TestBuilders.string;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.empty;
+import static reactor.core.publisher.Mono.just;
 
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
 class UserControllerTest {
 
     @MockBean
@@ -31,21 +37,28 @@ class UserControllerTest {
     private WebTestClient webTestClient;
 
     @MockBean(name = "centralRegistryJWKSet")
-    private JWKSet centralRegistryJWKSet;
+    JWKSet centralRegistryJWKSet;
+
+    @MockBean
+    CentralRegistryTokenVerifier centralRegistryTokenVerifier;
 
 
     @Test
-    void shouldFireAndForgetForConsentFetch() {
-        Mockito.when(patientSearchOrchestrator.processRequest(Mockito.any(), eq(X_CM_ID))).thenReturn(Mono.delay(Duration.ofSeconds(10)).then());
+    void shouldFireAndForgetForPatientsFind() {
+        var token = string();
+        var clientId = string();
+        when(patientSearchOrchestrator.processRequest(any(), eq(X_CM_ID), eq(clientId))).thenReturn(empty());
+        when(centralRegistryTokenVerifier.verify(token)).thenReturn(just(caller().clientId(clientId).build()));
 
-        WebTestClient mutatedWebTestClient = webTestClient.mutate().responseTimeout(Duration.ofSeconds(5)).build();
-        mutatedWebTestClient
+        webTestClient
                 .post()
                 .uri("/v1/patients/find")
-                .contentType(MediaType.APPLICATION_JSON)
+                .header(AUTHORIZATION, token)
+                .contentType(APPLICATION_JSON)
                 .bodyValue("{}")
                 .exchange()
-                .expectStatus().isAccepted();
+                .expectStatus()
+                .isAccepted();
     }
 
 }
