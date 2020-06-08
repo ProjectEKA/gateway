4e40e40a36d1e0645c90ffef02f66e7626a2ee9b
gayatrijagtap <44019996+gayatrijagtap@users.noreply.github.com>
2020-06-08 11:47:55 +0530

    [G_70] Gayatri | Implement /v1/patients/find (#22)
    * [G_70] Gayatri | Implement /v1/patients/find
    
    * [G_70] Gayatri | rename patientSearchServiceClient to patientSearchRequestOrchestrator in userController
    
    * [G_70] Gayatri | Implement removing temporary constant changes to patients/find
    
    * [G_70] Gayatri | Rename test

---
+++ src/main/java/in/projecteka/gateway/clients/PatientSearchServiceClient.java
@@ -0,0 +1,34 @@
+package in.projecteka.gateway.clients;
+
+import in.projecteka.gateway.common.CentralRegistry;
+import in.projecteka.gateway.common.cache.ServiceOptions;
+import in.projecteka.gateway.registry.BridgeRegistry;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static in.projecteka.gateway.registry.ServiceType.HIU;
+
+public class PatientSearchServiceClient extends ServiceClient{
+    private final BridgeRegistry bridgeRegistry;
+    private static final String REQUEST_ROUTE = "/v1/patients/find";
+    private static final String RESPONSE_ROUTE = "/v1/patients/on-find";
+
+    public PatientSearchServiceClient(ServiceOptions serviceOptions, WebClient.Builder webClientBuilder, CentralRegistry centralRegistry,BridgeRegistry bridgeRegistry) {
+        super(serviceOptions, webClientBuilder, centralRegistry);
+        this.bridgeRegistry = bridgeRegistry;
+    }
+
+    @Override
+    public Mono<Void> routeRequest(Map<String, Object> request, String url) {
+        return super.routeRequest(request, url + REQUEST_ROUTE);
+    }
+
+
+    @Override
+    protected Optional<String> getResponseUrl(String clientId) {
+        return bridgeRegistry.getConfigFor(clientId, HIU).map(host -> host + RESPONSE_ROUTE);
+    }
+}
---
+++ src/test/java/in/projecteka/gateway/user/UserControllerTest.java
@@ -0,0 +1,64 @@
+package in.projecteka.gateway.user;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import in.projecteka.gateway.clients.PatientSearchServiceClient;
+import in.projecteka.gateway.common.CentralRegistryTokenVerifier;
+import in.projecteka.gateway.common.RequestOrchestrator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static in.projecteka.gateway.common.Constants.X_CM_ID;
+import static in.projecteka.gateway.testcommon.TestBuilders.caller;
+import static in.projecteka.gateway.testcommon.TestBuilders.string;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.empty;
+import static reactor.core.publisher.Mono.just;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class UserControllerTest {
+
+    @MockBean
+    RequestOrchestrator<PatientSearchServiceClient> patientSearchOrchestrator;
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean(name = "centralRegistryJWKSet")
+    JWKSet centralRegistryJWKSet;
+
+    @MockBean
+    CentralRegistryTokenVerifier centralRegistryTokenVerifier;
+
+
+    @Test
+    void shouldFireAndForgetForPatientsFindInUserController() {
+        var token = string();
+        var clientId = string();
+        when(patientSearchOrchestrator.processRequest(any(), eq(X_CM_ID), eq(clientId))).thenReturn(empty());
+        when(centralRegistryTokenVerifier.verify(token)).thenReturn(just(caller().clientId(clientId).build()));
+
+        webTestClient
+                .post()
+                .uri("/v1/patients/find")
+                .header(AUTHORIZATION, token)
+                .contentType(APPLICATION_JSON)
+                .bodyValue("{}")
+                .exchange()
+                .expectStatus()
+                .isAccepted();
+    }
+
+}
