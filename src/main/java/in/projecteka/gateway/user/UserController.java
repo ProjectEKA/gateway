2b88dd7d4e12343f35ae8015a63589e5ca5854ce
gayatrijagtap <44019996+gayatrijagtap@users.noreply.github.com>
2020-06-08 16:41:40 +0530

    [G_72] Gayatri | Implement /v1/patients/on-find API (#25)

--- src/main/java/in/projecteka/gateway/user/UserController.java
+++ src/main/java/in/projecteka/gateway/user/UserController.java
@@ -3,6 +3,7 @@ package in.projecteka.gateway.user;
 import in.projecteka.gateway.clients.Caller;
 import in.projecteka.gateway.clients.PatientSearchServiceClient;
 import in.projecteka.gateway.common.RequestOrchestrator;
+import in.projecteka.gateway.common.ResponseOrchestrator;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -13,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import static in.projecteka.gateway.common.Constants.X_CM_ID;
+import static in.projecteka.gateway.common.Constants.X_HIU_ID;
 
 @RestController
 @AllArgsConstructor
 public class UserController {
     RequestOrchestrator<PatientSearchServiceClient> patientSearchRequestOrchestrator;
+    ResponseOrchestrator patientSearchResponseOrchestrator;
 
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping("/v1/patients/find")
@@ -26,5 +29,11 @@ public class UserController {
                 .map(securityContext -> (Caller) securityContext.getAuthentication().getPrincipal())
                 .map(Caller::getClientId)
                 .flatMap(clientId -> patientSearchRequestOrchestrator.handleThis(requestEntity, X_CM_ID, clientId));
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("/v1/patients/on-find")
+    public Mono<Void> onFindPatient(HttpEntity<String> requestEntity) {
+        return patientSearchResponseOrchestrator.processResponse(requestEntity, X_HIU_ID);
     }
 }
--- src/test/java/in/projecteka/gateway/user/UserControllerTest.java
+++ src/test/java/in/projecteka/gateway/user/UserControllerTest.java
@@ -1,21 +1,37 @@
 package in.projecteka.gateway.user;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.nimbusds.jose.jwk.JWKSet;
 import in.projecteka.gateway.clients.PatientSearchServiceClient;
 import in.projecteka.gateway.common.CentralRegistryTokenVerifier;
 import in.projecteka.gateway.common.RequestOrchestrator;
+import in.projecteka.gateway.common.ResponseOrchestrator;
+import in.projecteka.gateway.common.ValidatedResponse;
+import in.projecteka.gateway.common.ValidatedResponseAction;
+import in.projecteka.gateway.common.Validator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.util.UUID;
+
 import static in.projecteka.gateway.common.Constants.X_CM_ID;
+import static in.projecteka.gateway.common.Constants.REQUEST_ID;
+import static in.projecteka.gateway.common.Constants.X_HIU_ID;
 import static in.projecteka.gateway.testcommon.TestBuilders.caller;
 import static in.projecteka.gateway.testcommon.TestBuilders.string;
+import static in.projecteka.gateway.testcommon.TestEssentials.OBJECT_MAPPER;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
@@ -42,6 +58,20 @@ class UserControllerTest {
     @MockBean
     CentralRegistryTokenVerifier centralRegistryTokenVerifier;
 
+    @MockBean
+    @Qualifier("patientSearchResponseAction")
+    ValidatedResponseAction validatedResponseAction;
+
+    @Captor
+    ArgumentCaptor<JsonNode> jsonNodeArgumentCaptor;
+
+    @Qualifier("patientSearchResponseOrchestrator")
+    @MockBean
+    ResponseOrchestrator patientSearchResponseOrchestrator;
+
+    @MockBean
+    Validator patientSearchValidator;
+
     @Test
     void shouldFireAndForgetForPatientsFindInUserController() {
         var token = string();
@@ -52,6 +82,35 @@ class UserControllerTest {
         webTestClient
                 .post()
                 .uri("/v1/patients/find")
+                .header(AUTHORIZATION, token)
+                .contentType(APPLICATION_JSON)
+                .bodyValue("{}")
+                .exchange()
+                .expectStatus()
+                .isAccepted();
+    }
+
+    @Test
+    void shouldFireAndForgetForPatientOnFind() throws JsonProcessingException {
+        var requestId = UUID.randomUUID().toString();
+        var callerRequestId = UUID.randomUUID().toString();
+        var objectNode = OBJECT_MAPPER.createObjectNode();
+        var testId = string();
+        var token = string();
+        objectNode.put(REQUEST_ID, requestId);
+        ObjectNode respNode = OBJECT_MAPPER.createObjectNode();
+        respNode.put(REQUEST_ID, callerRequestId);
+        objectNode.set("resp", respNode);
+        var requestEntity = new HttpEntity<>(OBJECT_MAPPER.writeValueAsString(objectNode));
+        when(centralRegistryTokenVerifier.verify(token)).thenReturn(just(caller().build()));
+        when(patientSearchValidator.validateResponse(requestEntity, X_HIU_ID))
+                .thenReturn(just(new ValidatedResponse(testId, callerRequestId, objectNode)));
+        when(validatedResponseAction.execute(eq(X_HIU_ID), eq(testId), jsonNodeArgumentCaptor.capture()))
+                .thenReturn(empty());
+
+        webTestClient
+                .post()
+                .uri("/v1/patients/on-find")
                 .header(AUTHORIZATION, token)
                 .contentType(APPLICATION_JSON)
                 .bodyValue("{}")
