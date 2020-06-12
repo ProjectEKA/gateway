4f09f7164df3038f8a280a2ccb0d5db7f6b492dc
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-06-12 09:49:51 +0530

    GATEWAY ### | Removes unused imports

--- src/test/java/in/projecteka/gateway/clients/ServiceClientTest.java
+++ src/test/java/in/projecteka/gateway/clients/ServiceClientTest.java
@@ -23,7 +23,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.when;
 import static reactor.core.publisher.Mono.empty;
 import static reactor.core.publisher.Mono.just;
