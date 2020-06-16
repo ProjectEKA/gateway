439f3b3eb2af9c73ca5e041bd1d6b89ee42d30cd
nexengineer <nexengineer@gmail.com>
2020-06-16 23:31:13 +0530

    Adding condensed queue defination

--- rabbitmqsetup/import_defintions.sh
+++ rabbitmqsetup/import_defintions.sh
@@ -1,3 +1,3 @@
-rabbitmq_container_id=$(docker ps -aqf "name=centos_rabbitmq_1")
+rabbitmq_container_id=$(docker ps -aqf "name=rabbitmq")
 docker cp ./rabbitmq_defns_with_gw.json $rabbitmq_container_id:/tmp
 docker exec -it $rabbitmq_container_id /bin/bash -c "rabbitmqctl import_definitions /tmp/rabbitmq_defns_with_gw.json"
