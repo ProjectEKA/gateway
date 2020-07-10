ddf1ec5ea928c30c2b2885ecda144e10f3973588
Aditi Aggarwal <aditiaggarwal@Aditis-MacBook-Pro.local>
2020-07-10 13:09:52 +0530

    [GATEWAY_631] | Gayatri/Aditi | Standardize Error Codes

--- src/main/java/in/projecteka/gateway/clients/model/ErrorCode.java
+++ src/main/java/in/projecteka/gateway/clients/model/ErrorCode.java
@@ -6,8 +6,8 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.util.Arrays;
 
 public enum ErrorCode {
-    UNKNOWN_ERROR_OCCURRED(1000),
-    INVALID_TOKEN(1017),
+    UNKNOWN_ERROR_OCCURRED(2500),
+    INVALID_TOKEN(2401),
     SERVICE_DOWN(4222),
     TOO_MANY_REQUESTS_FOUND(2429);
     private final int value;
