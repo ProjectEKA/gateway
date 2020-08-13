da167515a8ee944fff200e7eb81d00ae7fd303b6
tw-mahendra <60882493+tw-mahendra@users.noreply.github.com>
2020-08-13 13:05:16 +0530

    EKA-831 | Neeraj/Mahendra | Adds type in service properties in mapping repository for service metrics (#93)

--- src/main/java/in/projecteka/gateway/common/MappingController.java
+++ src/main/java/in/projecteka/gateway/common/MappingController.java
@@ -2,6 +2,8 @@ package in.projecteka.gateway.common;
 
 import in.projecteka.gateway.common.model.Service;
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -9,12 +11,13 @@ import reactor.core.publisher.Mono;
 @RestController
 @AllArgsConstructor
 public class MappingController {
+    private static final Logger logger = LoggerFactory.getLogger(MappingController.class);
 
     private MappingService mappingService;
 
     @GetMapping(Constants.PATH_SERVICE_URLS)
     public Mono<Service> fetchAllServiceUrls() {
+        logger.info("Request for getting URLs: {}",Constants.PATH_SERVICE_URLS );
         return mappingService.fetchDependentServiceUrls();
-
     }
 }
