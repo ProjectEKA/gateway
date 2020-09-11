ed880eb7fd3346cf81c27a50cda2998939f2dc40
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-09-11 17:07:12 +0530

    #970 | Dheeraj | Introduce APIs to fetch auth modes for given healthID

--- src/main/java/in/projecteka/gateway/user/UserController.java
+++ src/main/java/in/projecteka/gateway/user/UserController.java
@@ -1,6 +1,7 @@
 package in.projecteka.gateway.user;
 
 import in.projecteka.gateway.clients.AuthConfirmServiceClient;
+import in.projecteka.gateway.clients.AuthModeFetchClient;
 import in.projecteka.gateway.clients.PatientSearchServiceClient;
 import in.projecteka.gateway.common.Caller;
 import in.projecteka.gateway.common.RequestOrchestrator;
@@ -14,7 +15,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+import java.util.Objects;
+
 import static in.projecteka.gateway.common.Constants.API_CALLED;
+import static in.projecteka.gateway.common.Constants.PATH_FETCH_AUTH_MODES;
+import static in.projecteka.gateway.common.Constants.PATH_HEALTH_INFORMATION_NOTIFY;
+import static in.projecteka.gateway.common.Constants.PATH_ON_FETCH_AUTH_MODES_TO_HIP;
+import static in.projecteka.gateway.common.Constants.PATH_ON_FETCH_AUTH_MODES_TO_HIU;
 import static in.projecteka.gateway.common.Constants.PATH_PATIENTS_FIND;
 import static in.projecteka.gateway.common.Constants.PATH_PATIENTS_ON_FIND;
 import static in.projecteka.gateway.common.Constants.USERS_AUTH_CONFIRM;
@@ -32,6 +39,8 @@ public class UserController {
     ResponseOrchestrator patientSearchResponseOrchestrator;
     RequestOrchestrator<AuthConfirmServiceClient> authConfirmRequestOrchestrator;
     ResponseOrchestrator authConfirmResponseOrchestrator;
+    RequestOrchestrator<AuthModeFetchClient> authModeFetchRequestOrchestrator;
+    ResponseOrchestrator authModeFetchResponseOrchestrator;
 
     @ResponseStatus(HttpStatus.ACCEPTED)
     @PostMapping(PATH_PATIENTS_FIND)
@@ -66,6 +75,43 @@ public class UserController {
     @PostMapping(USERS_AUTH_ON_CONFIRM)
     public Mono<Void> authOnConfirm(HttpEntity<String> requestEntity){
         return authConfirmResponseOrchestrator.processResponse(requestEntity, X_HIP_ID)
-                .subscriberContext(context -> context.put(API_CALLED, USERS_AUTH_ON_CONFIRM));    }
+                .subscriberContext(context -> context.put(API_CALLED, USERS_AUTH_ON_CONFIRM));
+    }
 
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping(PATH_FETCH_AUTH_MODES)
+    public Mono<Void> fetchAuthModes(HttpEntity<String> requestEntity){
+        return ReactiveSecurityContextHolder.getContext()
+                .map(securityContext -> (Caller) securityContext.getAuthentication().getPrincipal())
+                .map(Caller::getClientId)
+                .flatMap(clientId -> {
+                    if (isRequestFromHIU(requestEntity))
+                        return authModeFetchRequestOrchestrator
+                                .handleThis(requestEntity, X_CM_ID, X_HIU_ID, bridgeId(clientId))
+                                .subscriberContext(context -> context.put(API_CALLED, PATH_FETCH_AUTH_MODES));
+                    else
+                        return authModeFetchRequestOrchestrator
+                                .handleThis(requestEntity, X_CM_ID, X_HIP_ID, bridgeId(clientId))
+                                .subscriberContext(context -> context.put(API_CALLED, PATH_FETCH_AUTH_MODES));
+                });
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping(PATH_ON_FETCH_AUTH_MODES_TO_HIP)
+    public Mono<Void> onFetchAuthModesHIP(HttpEntity<String> requestEntity){
+        return authModeFetchResponseOrchestrator.processResponse(requestEntity, X_HIP_ID)
+                .subscriberContext(context -> context.put(API_CALLED, PATH_ON_FETCH_AUTH_MODES_TO_HIP));
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping(PATH_ON_FETCH_AUTH_MODES_TO_HIU)
+    public Mono<Void> onFetchAuthModesHIU(HttpEntity<String> requestEntity){
+        return authModeFetchResponseOrchestrator.processResponse(requestEntity, X_HIU_ID)
+                .subscriberContext(context -> context.put(API_CALLED, PATH_ON_FETCH_AUTH_MODES_TO_HIU));
+    }
+
+    private boolean isRequestFromHIU(HttpEntity<String> requestEntity) {
+        return requestEntity.hasBody() && Objects.requireNonNull(requestEntity.getBody())
+                .replaceAll("\\s+", "").contains("\"requester\":{\"type\":\"HIU\",");
+    }
 }
