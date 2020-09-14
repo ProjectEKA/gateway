d3500c3b255d56ed4f8ce76360b91c33ffb9611a
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2020-09-14 11:41:18 +0530

    #970| Dheeraj | Add APIs to fetch auth modes (#116)
    * #970 | Dheeraj | Introduce APIs to fetch auth modes for given healthID
    
    * #970 | Dheeraj | Refactor on-fetch-modes API
    
    * #970 | Dheeraj | Add beans for service clients and orchestrators
    
    * #970 | Dheeraj | Ignore case for checking requests

---
+++ src/main/java/in/projecteka/gateway/clients/AuthModeFetchClient.java
@@ -0,0 +1,42 @@
+package in.projecteka.gateway.clients;
+
+import in.projecteka.gateway.common.Constants;
+import in.projecteka.gateway.common.IdentityService;
+import in.projecteka.gateway.common.cache.ServiceOptions;
+import in.projecteka.gateway.registry.BridgeRegistry;
+import in.projecteka.gateway.registry.CMRegistry;
+import in.projecteka.gateway.registry.ServiceType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+public class AuthModeFetchClient extends ServiceClient {
+    private final BridgeRegistry bridgeRegistry;
+    private final CMRegistry cmRegistry;
+
+    public AuthModeFetchClient(ServiceOptions serviceOptions,
+                               WebClient.Builder webClientBuilder,
+                               IdentityService identityService,
+                               BridgeRegistry bridgeRegistry,
+                               CMRegistry cmRegistry) {
+        super(serviceOptions, webClientBuilder, identityService);
+        this.bridgeRegistry = bridgeRegistry;
+        this.cmRegistry = cmRegistry;
+    }
+
+    private Mono<String> getHIUHost(String clientId){
+        return bridgeRegistry.getHostFor(clientId, ServiceType.HIU)
+                .map(host -> host + Constants.PATH_BRIDGE_ON_FETCH_AUTH_MODES);
+    }
+
+    @Override
+    protected Mono<String> getResponseUrl(String clientId) {
+        return bridgeRegistry.getHostFor(clientId, ServiceType.HIP)
+                .switchIfEmpty(this.getHIUHost(clientId))
+                .map(host -> host + Constants.PATH_BRIDGE_ON_FETCH_AUTH_MODES);
+    }
+
+    @Override
+    protected Mono<String> getRequestUrl(String clientId) {
+        return cmRegistry.getHostFor(clientId).map(host -> host + Constants.PATH_FETCH_AUTH_MODES);
+    }
+}
