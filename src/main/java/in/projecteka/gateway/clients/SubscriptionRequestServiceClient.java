e4987dfcf7782eb822dbec5d170e90cc8985d601
Mritunjay Dubey <mddubey409@gmail.com>
2020-10-13 22:02:44 +0530

    Dubey |HL-1024| Subscription Init and OnInit (#132)
    * Dubey | HL-1024 | Introduce subscription init and on-init APIs
    
    * Dubey | 1024 | Subscription Init and OnInit

---
+++ src/main/java/in/projecteka/gateway/clients/SubscriptionRequestServiceClient.java
@@ -0,0 +1,36 @@
+package in.projecteka.gateway.clients;
+
+import in.projecteka.gateway.common.Constants;
+import in.projecteka.gateway.common.IdentityService;
+import in.projecteka.gateway.common.cache.ServiceOptions;
+import in.projecteka.gateway.registry.BridgeRegistry;
+import in.projecteka.gateway.registry.CMRegistry;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import static in.projecteka.gateway.registry.ServiceType.HIU;
+
+public class SubscriptionRequestServiceClient extends ServiceClient {
+    private final BridgeRegistry bridgeRegistry;
+    private final CMRegistry cmRegistry;
+
+    public SubscriptionRequestServiceClient(ServiceOptions serviceOptions,
+                                            WebClient.Builder webClientBuilder,
+                                            IdentityService identityService,
+                                            BridgeRegistry bridgeRegistry,
+                                            CMRegistry cmRegistry) {
+        super(serviceOptions, webClientBuilder, identityService);
+        this.bridgeRegistry = bridgeRegistry;
+        this.cmRegistry = cmRegistry;
+    }
+
+    @Override
+    protected Mono<String> getRequestUrl(String clientId) {
+        return cmRegistry.getHostFor(clientId).map(host -> host + Constants.PATH_SUBSCRIPTION_REQUESTS_INIT);
+    }
+
+    @Override
+    protected Mono<String> getResponseUrl(String clientId) {
+        return bridgeRegistry.getHostFor(clientId, HIU).map(host -> host + Constants.PATH_SUBSCRIPTION_REQUESTS_ON_INIT);
+    }
+}
---
+++ src/test/java/in/projecteka/gateway/subscription/SubscriptionControllerTest.java
@@ -0,0 +1,114 @@
+package in.projecteka.gateway.subscription;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.nimbusds.jose.jwk.JWKSet;
+import in.projecteka.gateway.clients.SubscriptionRequestServiceClient;
+import in.projecteka.gateway.common.Authenticator;
+import in.projecteka.gateway.common.Constants;
+import in.projecteka.gateway.common.RequestOrchestrator;
+import in.projecteka.gateway.common.ResponseOrchestrator;
+import in.projecteka.gateway.common.ValidatedResponse;
+import in.projecteka.gateway.common.ValidatedResponseAction;
+import in.projecteka.gateway.common.Validator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+import java.util.UUID;
+
+import static in.projecteka.gateway.common.Constants.BRIDGE_ID_PREFIX;
+import static in.projecteka.gateway.common.Constants.REQUEST_ID;
+import static in.projecteka.gateway.common.Constants.X_CM_ID;
+import static in.projecteka.gateway.common.Constants.X_HIU_ID;
+import static in.projecteka.gateway.common.Role.CM;
+import static in.projecteka.gateway.common.Role.HIU;
+import static in.projecteka.gateway.testcommon.TestBuilders.caller;
+import static in.projecteka.gateway.testcommon.TestBuilders.string;
+import static in.projecteka.gateway.testcommon.TestEssentials.OBJECT_MAPPER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentCaptor.forClass;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static reactor.core.publisher.Mono.empty;
+import static reactor.core.publisher.Mono.just;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class SubscriptionControllerTest {
+    @MockBean
+    RequestOrchestrator<SubscriptionRequestServiceClient> requestOrchestrator;
+
+    @Qualifier("subscriptionResponseOrchestrator")
+    @MockBean
+    ResponseOrchestrator subscriptionResponseOrchestrator;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    @MockBean(name = "centralRegistryJWKSet")
+    JWKSet centralRegistryJWKSet;
+
+    @MockBean
+    Validator subscriptionRequestValidator;
+
+    @MockBean
+    @Qualifier("subscriptionResponseAction")
+    ValidatedResponseAction validatedResponseAction;
+
+    @MockBean
+    Authenticator authenticator;
+
+    @Test
+    void shouldFireAndForgetForSubscriptionRequestInit() {
+        var token = string();
+        var clientId = string();
+        when(authenticator.verify(token))
+                .thenReturn(just(caller().clientId(clientId).roles(List.of(HIU)).build()));
+        when(requestOrchestrator
+                .handleThis(any(), eq(X_CM_ID), eq(X_HIU_ID), eq(BRIDGE_ID_PREFIX + clientId)))
+                .thenReturn(empty());
+
+        webTestClient
+                .post()
+                .uri(Constants.PATH_SUBSCRIPTION_REQUESTS_INIT)
+                .header(AUTHORIZATION, token)
+                .contentType(APPLICATION_JSON)
+                .bodyValue("{}")
+                .exchange()
+                .expectStatus()
+                .isAccepted();
+    }
+
+    @Test
+    void shouldFireAndForgetForSubscriptionRequestOnInit() {
+        var token = string();
+        when(authenticator.verify(token)).thenReturn(just(caller().roles(List.of(CM)).build()));
+        when(subscriptionResponseOrchestrator.processResponse(any(), eq(X_HIU_ID))).thenReturn(empty());
+
+        webTestClient
+                .post()
+                .uri(Constants.PATH_SUBSCRIPTION_REQUESTS_ON_INIT)
+                .header(AUTHORIZATION, token)
+                .contentType(APPLICATION_JSON)
+                .bodyValue("{}")
+                .exchange()
+                .expectStatus()
+                .isAccepted();
+    }
+}
